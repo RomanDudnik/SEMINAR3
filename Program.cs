c749bb67f5d3196fd9040ab11cbf6a7001cd040d
«Roman <research777@mail.ru>
2022-11-18 10:38:56 +0300

    SEMINAR3_Ex2

--- Program.cs
+++ Program.cs
@@ -25,3 +25,22 @@ Funkt(xCoord, yCoord);
 //Напишите программу, которая принимает на вход число (N) и 
 // выдаёт таблицу квадратов чисел от 1 до N. 5 -> 1, 4, 9, 16, 25. 2 -> 1,4
 
+void PssibleValue(int n)
+{
+    if (n == 1)
+        Console.WriteLine("1st quater X > 0, Y > 0");
+    else if (n ==2)
+        Console.WriteLine("2st quater X < 0, Y > 0");
+    else if (n ==2)
+        Console.WriteLine("3st quater X < 0, Y < 0");
+    else Console.WriteLine("4st quater X > 0, Y < 0");
+}
+
+Console.WriteLine("input number of quater: ");
+int numQuater = Convert.ToInt32(Console.ReadLine());
+
+if (numQuater => 1 && numQater)
+    PossibleValue(numQater);
+else Console.WriteLine ("its not number of quater!");
+
+
